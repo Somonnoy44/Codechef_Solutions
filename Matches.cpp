6d8780d74d135ff6ca8f4cb314271c7e88290f88
Somonnoy44 <51916219+Somonnoy44@users.noreply.github.com>
2019-10-13 19:05:51 +0530

    Initial commit
    Added the solution of the problem on Matches (September Mega Cook Off 2019 Division 2)

---
+++ Matches.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int T;
+	int A,B;
+    cin>>T;
+    cin>>A>>B;
+    int arr[10];
+    arr[0]=6;
+    arr[1]=2;
+    arr[2]=5;
+    arr[3]=5;
+    arr[4]=4;
+    arr[5]=5;
+    arr[6]=6;
+    arr[7]=3;
+    arr[8]=7;
+    arr[9]=6;
+	int sum=A+B;
+	int s;
+	for(int i=sum;i>=0;i=i/10)
+	{
+		int digit=i%10;
+		s=arr[digit];
+	}
+    return 0;
+    cout<<s<<endl;
+}
